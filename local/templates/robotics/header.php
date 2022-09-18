3e79dd656c492417a7f488bbef10210b4ec27ea6
Roman <r__kasatkin@mail.ru>
2022-09-18 14:17:42 +0500

    change header and footer

--- local/templates/robotics/header.php
+++ local/templates/robotics/header.php
@@ -72,7 +72,7 @@ Loc::loadLanguageFile(__FILE__);
                     "ALLOW_MULTI_SELECT" => "N",
                     "CHILD_MENU_TYPE" => "left",
                     "DELAY" => "N",
-                    "MAX_LEVEL" => "3",
+                    "MAX_LEVEL" => "2",
                     "MENU_CACHE_GET_VARS" => array(""),
                     "MENU_CACHE_TIME" => "3600",
                     "MENU_CACHE_TYPE" => "N",

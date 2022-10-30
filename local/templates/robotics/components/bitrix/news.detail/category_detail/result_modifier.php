7ee170acf4d6cbb00d894a9b528bb079cc2e1115
Roman <r__kasatkin@mail.ru>
2022-10-30 12:39:25 +0500

    result modif change  filter

--- local/templates/robotics/components/bitrix/news.detail/category_detail/result_modifier.php
+++ local/templates/robotics/components/bitrix/news.detail/category_detail/result_modifier.php
@@ -3,13 +3,19 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
+$isCategories = explode('/', $APPLICATION->GetCurPage())[1] === 'categories';
+$filter = [
+    "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+];
+if ($isCategories) {
+    $filter["!CODE"] = $GLOBALS[$arParams["FILTER_NAME"]]["SECTION_CODE"];
+} else {
+    $filter["!ID"] = $GLOBALS[$arParams["FILTER_NAME"]]["SECTION_ID"];
+}
 $arResult["TAGS"] = [];
 $res = CIBlockSection::GetList(
     ["SORT"=>"ASC"],
-    [
-        "IBLOCK_ID" => $arParams["IBLOCK_ID"],
-        "!CODE" => $GLOBALS[$arParams["FILTER_NAME"]]["SECTION_CODE"]
-    ],
+    $filter,
     false,
     ['ID', 'NAME', 'SECTION_PAGE_URL'],
     false
